db0a03191dafbb34a76a88f991a70264bc24951f
Daniel Marino <starzonmyarmz@gmail.com>
2019-05-06 16:07:23 -0400

    Style the cart

--- source/javascripts/cart.js
+++ source/javascripts/cart.js
@@ -1,7 +1,7 @@
 document.addEventListener('click', (event) => {
   if (!event.target.closest('[data-remove]')) return
 
-  const product = event.target.closest('li')
+  const product = event.target.closest('tr')
   const input = product.querySelector('input[id$="_qty"]')
 
   input.value = 0
