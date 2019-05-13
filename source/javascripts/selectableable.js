00ac77a94ac220d43ceb5c3b7ae2c9c01735c0c0
Daniel Marino <starzonmyarmz@gmail.com>
2019-05-13 16:13:41 -0400

    Style selectableable

--- source/javascripts/selectableable.js
+++ source/javascripts/selectableable.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
   // Hide the #options element
-  select.style.visibility = 'hidden'
+  select.classList.add('options-are-hidden')
 
 
 
@@ -40,24 +40,28 @@ document.addEventListener('DOMContentLoaded', () => {
   let fieldsets = []
 
   for (let key of Object.keys(groups)) {
-    let fieldset = document.createElement('fieldset')
-    let legend = document.createElement('legend')
+    let fieldset = document.createElement('div')
+    let legend = document.createElement('div')
 
     legend.className = 'selectableable_legend'
     legend.innerHTML = key
 
-    fieldset.className = 'selectableable_fieldset'
+    fieldset.className = 'selectableable_fieldset selectableable_' + key
     fieldset.appendChild(legend)
 
     fieldsets.push(fieldset)
 
     for (let value of groups[key]) {
+      let choice  = document.createElement('div')
       let input = document.createElement('input')
       let label = document.createElement('label')
+
+      choice.className = 'selectableable_choice'
 
       input.type = 'radio'
       input.name = 'selectableable_' + key
       input.value = value
+      input.id = 'selectableable_' + value.replace(/\s/, '_')
       input.className = 'selectableable_input'
 
       input.addEventListener('click', () => {
@@ -66,10 +70,12 @@ document.addEventListener('DOMContentLoaded', () => {
       })
 
       label.className = 'selectableable_label'
-      label.appendChild(input)
-      label.appendChild(document.createTextNode(value))
+      label.htmlFor = 'selectableable_' + value.replace(/\s/, '_')
 
-      fieldset.appendChild(label)
+      choice.appendChild(input)
+      label.appendChild(document.createTextNode(value))
+      choice.appendChild(label)
+      fieldset.appendChild(choice)
     }
 
     document.getElementById('product-options').appendChild(fieldset)
