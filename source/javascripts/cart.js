762af694cc2c775e95b756bbe4d31de976aee05e
Daniel Marino <starzonmyarmz@gmail.com>
2019-12-06 15:51:59 -0500

    Janky way to show color on cart

--- source/javascripts/cart.js
+++ source/javascripts/cart.js
@@ -1,3 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('tr.cart-item').forEach((row) => {
+    const pair = row.querySelector('[data-option]').innerText.split(',')[0].toLowerCase()
+
+    if (pair.includes('color')) {
+      row.querySelectorAll('img').forEach((img) => img.hidden = !img.src.includes(pair.split(': ')[1]))
+    } else {
+      row.querySelector('img').hidden = false
+    }
+  })
+})
+
 document.addEventListener('click', (event) => {
   if (!event.target.closest('[data-remove]')) return
 
