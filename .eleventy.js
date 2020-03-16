1869c8bac36417d797e5d78ef5431d7f887788d0
Daniel Marino <starzonmyarmz@gmail.com>
2020-03-16 09:08:40 -0400

    Wire up sass

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,10 @@
+const pluginSass = require("eleventy-plugin-sass");
+
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addPlugin(pluginSass, {
+    watch: ['src/**/*.{scss,sass}', '!node_modules/**'],
+    sourcemaps: true
+  });
 
   eleventyConfig.addPassthroughCopy('src/images')
   eleventyConfig.addPassthroughCopy('src/javascripts')
