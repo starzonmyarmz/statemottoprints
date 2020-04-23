ff74d0b1e8103628b219e2d16cdda82736485ece
Daniel Marino <starzonmyarmz@gmail.com>
2020-04-23 09:50:42 -0400

    placeholder

--- source/javascripts/base.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('click', (event) => {
-  if (!event.target.closest('[data-toggle-nav]')) return
-  document.body.classList.toggle('navigation-visible')
-  document.getElementById('toggle-nav-menu').hidden = !document.getElementById('toggle-nav-menu').hidden
-  document.getElementById('toggle-nav-x').hidden = !document.getElementById('toggle-nav-x').hidden
-})
--- source/theme.js
+++
@@ -1,5 +0,0 @@
-//= require javascripts/base
-//= require javascripts/cart
-//= require javascripts/product
-//= require javascripts/slideshow
-//= require javascripts/selectableable
