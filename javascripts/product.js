5c5e4ef36fb1a06b9a5b3600c00eef3c4c0afa0e
starzonmyarmz <starzonmyarmz@users.noreply.github.com>
2020-05-07 14:05:58 +0000

    deploy: b60826ec26280c901f24ab0d42e0097da6abd256

--- javascripts/product.js
+++ javascripts/product.js
@@ -1,10 +1,10 @@
 const cartButton = document.getElementById('add-to-cart')
 
-document.addEventListener('change', ({target}) => {
-  if (!target.closest('[data-price]')) return
-  document.querySelector('.product-price-amount').innerText = target.dataset.price.replace('.00', '')
-  cartButton.dataset.itemPrice = target.dataset.price
-})
+// document.addEventListener('change', ({target}) => {
+//   if (!target.closest('[data-price]')) return
+//   document.querySelector('.product-price-amount').innerText = target.dataset.price.replace('.00', '')
+//   cartButton.dataset.itemPrice = target.dataset.price
+// })
 
 document.addEventListener('change', ({target}) => {
   if (!target.closest('[data-image]')) return
@@ -20,25 +20,41 @@ document.querySelectorAll('.product-thumbnail').forEach((thumbnail) => {
 })
 
 document.addEventListener('change', ({target}) => {
+  if (!target.closest('[name="product-choice-dimension"]')) return
+  cartButton.dataset.itemCustom1Value = target.value
+})
+
+document.addEventListener('change', ({target}) => {
+  if (!target.closest('[name="product-choice-color"]')) return
+  cartButton.dataset.itemCustom2Value = target.value
+})
+
+document.addEventListener('change', ({target}) => {
+  if (!target.closest('[name="product-choice-size"]')) return
+  cartButton.dataset.itemCustom3Value = target.value
+})
+
+
+document.addEventListener('change', ({target}) => {
   if (!target.closest('.product-choice-input')) return
   const fields = document.querySelectorAll('.product-choices').length
   const selected = document.querySelectorAll('.product-choice-input[type="radio"]:checked')
 
   if (fields === selected.length) {
     let productAttrs = []
-    selected.forEach((option) => productAttrs.push(option.value))
-
-    cartButton.dataset.itemId = `${cartButton.dataset.itemState}_${cartButton.dataset.itemType}_${productAttrs.join('_')}`
-    cartButton.dataset.itemName = `${document.querySelector('.product-title').innerText} ${productAttrs.join(' ')}`
     cartButton.disabled = false
   }
 })
 
-document.addEventListener('input', ({target}) => {
-  const input = target.closest('.product-choice-input')
-  if (!input) return
-  const color = input.value
-  document.querySelectorAll('button.product-thumbnail').forEach((button) => {
-    button.dataset.source.includes(color) ? button.click() : ''
-  })
-})
+console.log(cartButton.dataset)
+
+
+// Change product image based on selected color
+// document.addEventListener('input', ({target}) => {
+//   const input = target.closest('.product-choice-input')
+//   if (!input) return
+//   const color = input.value
+//   document.querySelectorAll('button.product-thumbnail').forEach((button) => {
+//     button.dataset.source.includes(color) ? button.click() : ''
+//   })
+// })
