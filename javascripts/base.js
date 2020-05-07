897aa1d4f3e2b954a945dfd969bc3146dbea5167
starzonmyarmz <starzonmyarmz@users.noreply.github.com>
2020-05-07 20:08:36 +0000

    deploy: bf06de0efe2ea71f2676c03cc52c63683da06583

--- javascripts/base.js
+++ javascripts/base.js
@@ -4,3 +4,9 @@ document.addEventListener('click', (event) => {
   document.getElementById('toggle-nav-menu').hidden = !document.getElementById('toggle-nav-menu').hidden
   document.getElementById('toggle-nav-x').hidden = !document.getElementById('toggle-nav-x').hidden
 })
+
+const cart_count_observer = new MutationObserver((element) => {
+  const count = element[0].target
+  count.hidden = count.innerText === '0'
+})
+cart_count_observer.observe(document.querySelector('.cart-count'), { childList: true })
