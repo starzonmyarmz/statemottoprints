d45264069fc30d0767b836082b3bd681ef6db5b2
Daniel Marino <starzonmyarmz@gmail.com>
2020-05-11 14:25:10 -0400

    Show grey tees and white prints last

--- src/_data/helpers.js
+++ src/_data/helpers.js
@@ -1,9 +1,19 @@
 const glob = require('glob')
 const path = require('path')
 
+const regex = /_print_white|_tee_grey/
+
 module.exports = {
   productImages(state, type) {
-    return glob.sync(`./src/images/product/${state}_${type}_*.{jpg,png}`).map((name) => {
+    return glob.sync(`./src/images/product/${state}_${type}_*.{jpg,png}`).sort((a, b) => {
+      if (regex.test(a)) {
+        return 1
+      } else if (regex.test(b)) {
+        return -1
+      } else {
+        return 0
+      }
+    }).map((name) => {
       return '/' + path.relative('src', name)
     })
   }
